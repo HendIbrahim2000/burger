3a430644a3ccaeb654c7db2e3a2c8c5c3134d135
HendIbrahim2000 <hendibrahim2000@gmail.com>
2022-03-03 14:23:04 +0200

    Burger project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Layout>
+          <Routes>
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/" exact element={<BurgerBuilder />} />
+          </Routes>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
