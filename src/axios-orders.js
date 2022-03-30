4db8c515dcdeea6c87f1dcd898b3e8715eeefb7d
HendIbrahim2000 <hendibrahim2000@gmail.com>
2022-03-31 01:36:41 +0200

    logout

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://burger-630f1-default-rtdb.firebaseio.com/'
+    baseURL: 'https://burger-7b0c7-default-rtdb.firebaseio.com/'
 });
 
 export default instance;
